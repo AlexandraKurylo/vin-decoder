d57350f18cc20be08aaed4690577a06455ed29e1
Alexandra <aleksandrakurilo61@gmail.com>
2026-06-28 13:25:56 +0300

    VariableSummaryCard component

---
+++ src/components/VariableSummaryCard/VariableSummaryCard.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import cls from "./VariableSummaryCard.module.css";
+import type { IVariable } from "../../api/variables.api";
+
+export const VariableSummaryCard = ({ variable }: { variable: IVariable }) => (
+  <div className={cls.card}>
+    <h3 className={cls.title}>{variable.Name}</h3>
+    <p className={cls.group}>{variable.GroupName}</p>
+    <Link to={`/variables/${variable.ID}`} className={cls.detailsBtn}>
+      More information
+    </Link>
+  </div>
+);
